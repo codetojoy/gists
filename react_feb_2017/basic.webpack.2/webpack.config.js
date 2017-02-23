6187f9f807c920f832157672d24b0ca9db24b6c4
Michael Easter <codetojoy@gmail.com>
2017-02-22 22:56:03 -0400

    incremental change as part of a larger frenzy of activity

---
+++ react_feb_2017/basic.webpack.2/webpack.config.js
@@ -0,0 +1,38 @@
+'use strict';
+
+var path = require('path')
+
+var DIST_DIR = path.resolve(__dirname + '/dist') 
+var APP_DIR = path.resolve(__dirname + '/app')
+
+const HtmlPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    devServer: {
+        inline: true
+      , contentBase: DIST_DIR
+      , port: 3333 
+    },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['react', 'es2015']
+                }
+            }
+        ],
+    },
+    plugins: [new HtmlPlugin({
+        title: 'CustomTitle',
+        template: APP_DIR + '/index.html', // Load a custom template
+        inject: 'body' // Inject all scripts into the body
+    })],
+    entry: APP_DIR + '/index.jsx',
+    output: {
+        path: `${__dirname}/dist`,
+        filename: 'bundle.js'
+    }
+};
