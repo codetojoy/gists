7deff1b40510a2af9d96db422a2245bb7a3a6c48
Michael Easter <codetojoy@gmail.com>
2017-03-23 16:29:05 -0300

    new sandbox for war/jar example

---
+++ gradle_war_jar_mar_2017/app/api/src/main/webapp/index.jsp
@@ -0,0 +1,9 @@
+<%@page import="net.codetojoy.utils.Utils" %>
+
+<html>
+<body>
+<p3>hello there</p>
+<hr/>
+<p>version is <%= new Utils().getVersion() %></p>
+</body>
+</html>
---
+++ gradle_war_jar_mar_2017/lib/utils/src/main/java/net/codetojoy/util/Utils.java
@@ -0,0 +1,11 @@
+
+package net.codetojoy.utils;
+
+public class Utils {
+    private String version = "0.9.1";
+
+    public String getVersion() {
+        return version;
+    }
+}
+
