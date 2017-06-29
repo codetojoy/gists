2ab3de0085492b43466de6c7925fa2a719f85788
Michael Easter <codetojoy@gmail.com>
2017-06-29 00:16:36 -0300

    incremental change as part of a larger frenzy of activity

--- junit_parametric_jun_2017/src/test/java/net/codetojoy/StringUtilsTestCase.java
+++ junit_parametric_jun_2017/src/test/java/net/codetojoy/StringUtilsTestCase.java
@@ -17,29 +17,29 @@ public class StringUtilsTestCase {
     private String s;
     private String expected;
     private String note;
-    private String regex; 
+    private static final String Y = "Y";
+    private static final String N = "N";
 
-    public StringUtilsTestCase(String s, String expected, String note, String regex) {
+    public StringUtilsTestCase(String s, String expected, String note) {
         this.s = s;
         this.expected = expected;
         this.note = note;
-        this.regex = regex;
     }
 
-    @Parameters(name = "#{index} ____ {0} ____ {1} ____ {2} {3}")
+    @Parameters(name = "#{index} ____ {0} ____ {1} ____ {2}")
     public static Collection<Object[]> data() {
         return Arrays.asList(new Object[][]{
-{"d8bebfbc-aea2-4c13-9122-264217042fa3", "Y", "basic", "____ " + StringUtils.UUID_REGEX},
-{"d8bebfbc-aea2-4c13-9122-264217042fa3", "Y", "group3[0] is 4", ""},
-{"d8bebfbc-aea2-6c13-9122-264217042fa3", "N", "group3[0] is 4 !", ""},
-{"d8bebfbc-aea2-4c13-8122-264217042fa3", "Y", "group4[0] in 8,9,A,B", ""},
-{"d8bebfbc-aea2-4c13-2122-264217042fa3", "N", "group4[0] in 8,9,A,B !", ""},
+            {"d8bebfbc-aea2-4c13-9122-264217042fa3", Y, "basic",},
+            {"d8bebfbc-aea2-4c13-9122-264217042fa3", Y, "group3[0] is 4",},
+            {"d8bebfbc-aea2-6c13-9122-264217042fa3", N, "group3[0] is 4 !",},
+            {"d8bebfbc-aea2-4c13-8122-264217042fa3", Y, "group4[0] in 8,9,A,B",},
+            {"d8bebfbc-aea2-4c13-2122-264217042fa3", N, "group4[0] in 8,9,A,B !",},
         });
     }
 
     @Test
     public void testIsUUID() {
-        boolean expectedResult = (expected.equals("Y"));
+        boolean expectedResult = (expected.equals(Y));
 
         // test
         boolean result = StringUtils.isUUID(s);
