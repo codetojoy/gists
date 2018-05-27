7d903e97327c8b4b5eae32d7a084f1adec4377ea
Michael Easter <codetojoy@gmail.com>
2018-05-27 08:23:12 -0300

    first pass at multi-ENV tags

--- easy_war_may_2018/devops/git.commit.tag.artifact.maps.sh
+++ easy_war_may_2018/devops/git.commit.tag.artifact.maps.sh
@@ -3,6 +3,8 @@
 cd $1
 
 git add devops/DEV.tag.artifact.map.csv
+git add devops/QA.tag.artifact.map.csv
+git add devops/UAT.tag.artifact.map.csv
 
 git commit -m "automated commit for tag artifact map"
 git push origin master 
