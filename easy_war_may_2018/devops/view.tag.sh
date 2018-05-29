a060c0ac2ceaa4c6f76f13534605356063a74c86
Michael Easter <codetojoy@gmail.com>
2018-05-28 21:48:38 -0300

    tag annotation

---
+++ easy_war_may_2018/devops/view.tag.sh
@@ -0,0 +1,1 @@
+git tag -l -n9 easywar-1-0-0-SNAPSHOT-2018-05-28-2142
---
+++ easy_war_may_2018/tag_it_v2.sh
@@ -0,0 +1,20 @@
+#!/bin/bash 
+
+[ -z "$TRUNK_MAJOR_VERSION" ] && echo "please use setvars.sh" && exit 1;
+[ -z "$TRUNK_MINOR_VERSION" ] && echo "please use setvars.sh" && exit 1;
+[ -z "$TRUNK_INCREMENTAL_VERSION" ] && echo "please use setvars.sh" && exit 1;
+[ -z "$TRUNK_QUALIFIER" ] && echo "please use setvars.sh" && exit 1;
+
+MAJOR=$TRUNK_MAJOR_VERSION
+MINOR=$TRUNK_MINOR_VERSION
+INCREMENTAL=$TRUNK_INCREMENTAL_VERSION
+QUALIFIER=$TRUNK_QUALIFIER
+TIMESTAMP=`date '+%Y-%m-%d-%H%M'`
+TAG=easywar-${MAJOR}-${MINOR}-${INCREMENTAL}-${QUALIFIER}-${TIMESTAMP}
+
+echo "TRACER tag: ${TAG}"
+
+# TODO: confirm argument
+
+git tag $TAG -a -m "this is a test message"
+git push origin $TAG
