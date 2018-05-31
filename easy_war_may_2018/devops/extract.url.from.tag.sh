578cf7708e6d3e0a03d1f057b4746014460fbb4f
Michael Easter <codetojoy@gmail.com>
2018-05-31 07:04:34 -0300

    incremental change as part of a larger frenzy of activity

--- easy_war_may_2018/devops/extract.url.from.tag.sh
+++ easy_war_may_2018/devops/extract.url.from.tag.sh
@@ -2,17 +2,9 @@
 
 [ -z "$1" ] && echo "requires tag argument" && exit 1;
 [ -z "$2" ] && echo "requires ENV" && exit 1;
-[ -z "$3" ] && echo "requires out file" && exit 1;
 
 TAG=$1
 ENV=$2
-ARTIFACT_URL_FILE=$3
 
-if [ -f $ARTIfACT_URL_FILE ]; then
-    echo ""
-else
-    rm $ARTIFACT_URL_FILE
-fi
-
-git tag --list -n80 $TAG | grep "${ENV}.artifact.url" | awk -F'"' '{print $4}' > $ARTIFACT_URL_FILE
+git tag --list -n80 $TAG | grep "${ENV}.artifact.url" | awk -F'"' '{print $4}' 
  
