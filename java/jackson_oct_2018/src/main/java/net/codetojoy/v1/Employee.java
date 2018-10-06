907511a675758ed92e76982297681409ffaaa893
Michael Easter <codetojoy@gmail.com>
2018-10-05 21:11:54 -0300

    incremental change as part of a larger frenzy of activity

---
+++ java/jackson_oct_2018/src/main/java/net/codetojoy/v1/Employee.java
@@ -0,0 +1,29 @@
+
+package net.codetojoy.v1;
+
+import java.util.*;
+
+public class Employee {
+    private String name;
+    private String address;
+    private Integer id;
+    private List<String> contacts;
+    private List<Employee> reports;
+
+    // ----------- getter / setter
+
+    public List<String> getContacts() { return contacts; }
+    public void setContacts(List<String> contacts) { this.contacts = contacts; }
+
+    public List<Employee> getReports() { return reports; }
+    public void setReports(List<Employee> reports) { this.reports = reports; }
+
+    public String getName() { return name; }
+    public void setName(String name) { this.name = name; }
+
+    public String getAddress() { return address; }
+    public void setAddress(String address) { this.address = address; }
+
+    public Integer getId() { return id; }
+    public void setId(Integer id) { this.id = id; }
+}
---
+++ java/jackson_oct_2018/src/test/java/net/codetojoy/v1/UtilsTestCase.java
@@ -0,0 +1,83 @@
+
+package net.codetojoy.v1;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+import org.junit.*;
+import static org.junit.Assert.*;
+
+public class UtilsTestCase {
+    @Test
+    public void testCanary() {
+        // test
+        int result = new Utils().getFoo();
+
+        assertEquals(5150, result);
+    }
+
+    @Test
+    public void testBuildEmployee_String() throws Exception {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("{");
+        builder.append(" \"name\" : \"Mozart\", ");
+        builder.append(" \"address\" : \"Salzburg\", ");
+        builder.append(" \"id\" : 6160, ");
+        builder.append(" \"contacts\" : [\"x\", \"y\", \"z\"], ");
+        builder.append(" \"reports\" : [");
+        builder.append(" { \"name\" : \"report1\", \"id\" : 7170}, ");
+        builder.append(" { \"name\" : \"report2\", \"id\" : 8180}, ");
+        builder.append(" { \"name\" : \"report3\", \"id\" : 9190} ");
+        builder.append("] ");
+        builder.append("}");
+
+        String jsonStr = builder.toString();
+        InputStream inputStream = new ByteArrayInputStream(jsonStr.getBytes(StandardCharsets.UTF_8));
+
+        // test
+        Employee result = new Utils().buildEmployee(inputStream);
+
+        assertEquals("Mozart", result.getName());
+        assertEquals("Salzburg", result.getAddress());
+        assertEquals((Integer) 6160, result.getId());
+
+        assertEquals(3, result.getContacts().size());
+        assertEquals("x", result.getContacts().get(0));
+
+        assertEquals(3, result.getReports().size());
+        assertEquals((Integer) 7170, result.getReports().get(0).getId());
+    }
+
+    @Test
+    public void testBuildEmployee_InputStream() throws Exception {
+        StringBuilder builder = new StringBuilder();
+
+        builder.append("{");
+        builder.append(" \"name\" : \"Mozart\", ");
+        builder.append(" \"address\" : \"Salzburg\", ");
+        builder.append(" \"id\" : 6160, ");
+        builder.append(" \"contacts\" : [\"x\", \"y\", \"z\"], ");
+        builder.append(" \"reports\" : [");
+        builder.append(" { \"name\" : \"report1\", \"id\" : 7170}, ");
+        builder.append(" { \"name\" : \"report2\", \"id\" : 8180}, ");
+        builder.append(" { \"name\" : \"report3\", \"id\" : 9190} ");
+        builder.append("] ");
+        builder.append("} ");
+
+        String jsonStr = builder.toString();
+
+        // test
+        Employee result = new Utils().buildEmployee(jsonStr);
+
+        assertEquals("Mozart", result.getName());
+        assertEquals("Salzburg", result.getAddress());
+        assertEquals((Integer) 6160, result.getId());
+
+        assertEquals(3, result.getContacts().size());
+        assertEquals("x", result.getContacts().get(0));
+
+        assertEquals(3, result.getReports().size());
+        assertEquals((Integer) 7170, result.getReports().get(0).getId());
+    }
+}
