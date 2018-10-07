0fbbc25dca490bb3d0869045eeb48bb5ff163c6e
Michael Easter <codetojoy@gmail.com>
2018-10-07 16:52:23 -0300

    use const ... still unclear which version is supported

--- maven/testing_JS_oct_2018/src/test/jasmine/demoSpec.js
+++ maven/testing_JS_oct_2018/src/test/jasmine/demoSpec.js
@@ -3,13 +3,10 @@ describe("demo", function() {
         it("canary test", function() {
             expect(3+5).toEqual(8);
         });
-        it("canary test 2", function() {
-            var s = "abc";
-            expect(s).toEqual("abc");
-        });
+        
         it("demo reverse", function() {
-            var s = "abc123";
-            var result = myReverse(s);
+            const s = "abc123";
+            const result = myReverse(s);
             expect(result).toEqual("321cba");
         });
     });
