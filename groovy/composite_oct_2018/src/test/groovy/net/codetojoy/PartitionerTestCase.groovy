3dceb993c11279baafcb5ca63bbe67adbfb613a3
Michael Easter <codetojoy@gmail.com>
2018-10-21 16:20:20 -0300

    new test case

--- groovy/composite_oct_2018/src/test/groovy/net/codetojoy/PartitionerTestCase.groovy
+++ groovy/composite_oct_2018/src/test/groovy/net/codetojoy/PartitionerTestCase.groovy
@@ -11,4 +11,23 @@ class PartitionerTestCase {
     void testCanary() {
         assert 4 == 2 + 2
     }
+
+    @Test
+    void testParitionByGroup() {
+        def rows = new Rows().getRows()
+        def questions = new QuestionBuilder().transformAndCollectAnswers(rows)
+
+        // test
+        def results = partitioner.partitionByGroup(questions)
+
+        assert 3 == results.size()
+
+        assert 1 == results.get(0).size()
+        assert 2 == results.get(1).size()
+        assert 3 == results.get(2).size()
+
+        assert 1 == results.get(0).get(0).getGroup()
+        assert 2 == results.get(1).get(0).getGroup()
+        assert 3 == results.get(2).get(0).getGroup()
+    }
 }
