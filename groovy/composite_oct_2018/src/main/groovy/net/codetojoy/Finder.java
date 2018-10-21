3210743364c46789e2258c9765dca56b7156b68c
Michael Easter <codetojoy@gmail.com>
2018-10-21 05:17:41 -0300

    refactor

--- groovy/composite_oct_2018/src/main/groovy/net/codetojoy/Finder.java
+++ groovy/composite_oct_2018/src/main/groovy/net/codetojoy/Finder.java
@@ -1,16 +1,17 @@
 
 package net.codetojoy;
 
+import java.util.*;
 import java.util.stream.Collectors;
 
 public class Finder {
 
     public Question findQuestion(List<Question> questions, int group, int tier, int level) {
         Question result = questions.stream()
-                                   .filter{ q -> q.getGroup() == group &&
+                                   .filter( q -> q.getGroup() == group &&
                                                  q.getTier() == tier &&
                                                  q.getLevel() == level
-                                          }
+                                          )
                                    .findFirst()
                                    .orElse(null);
 
@@ -21,16 +22,16 @@ public class Finder {
         Question question = null;
 
         if (tier == 2 && level == 1) {
-            question = findQuestion(questions, group, tier - 1, level)
+            question = findQuestion(questions, group, tier - 1, level);
         } else if (tier == 2 && level == 2) {
             for (Question tier1Question : questions) {
-                question = findQuestion(tier1Question.getSubQuestions(), group, tier, level  - 1)
+                question = findQuestion(tier1Question.getSubQuestions(), group, tier, level  - 1);
                 if (question != null) {
                     break;
                 }
             }
         } else {
-            throw new IllegalArgumentException("internal error")
+            throw new IllegalArgumentException("internal error");
         }
 
         return question;
