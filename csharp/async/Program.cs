4bd3726bbb9020c623e5afb2b1ec6ece190d852a
Michael Easter <codetojoy@gmail.com>
2019-08-04 19:49:08 -0300

    new gists for C#

---
+++ csharp/async/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace async
+{
+    class Program
+    {
+        readonly HttpClient client = new HttpClient(); 
+
+        async Task<int> GetPageLengthAsync(string url)
+        {    
+            Task<string> fetchTask = client.GetStringAsync(url);    
+            int length = (await fetchTask).Length;    
+            return length;
+        }
+        void Go()
+        {    
+            Task<int> lengthTask = GetPageLengthAsync("http://peidevs.github.io");    
+            Console.WriteLine(lengthTask.Result);
+        }
+
+        static void Main(string[] args)
+        {
+            var program = new Program();
+            program.Go(); 
+        }
+    }
+}
