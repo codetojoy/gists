ee1514478958db1769b85c7820db6cdaacc21ae6
Michael Easter <codetojoy@gmail.com>
2020-01-12 18:23:01 -0400

    simple utility to generate Markdown table

---
+++ groovy/csv_markdown_table/Converter.groovy
@@ -0,0 +1,84 @@
+
+@Grab('com.xlson.groovycsv:groovycsv:1.3')
+
+import static com.xlson.groovycsv.CsvParser.parseCsv
+
+/*
+| Tables        | Are           | Cool  |
+| ------------- |:-------------:| -----:|
+| col 3 is      | right-aligned | $1600 |
+*/
+
+def generateHeader = { def row, def rowMap, def numColumns -> 
+    def buffer = new StringBuilder() 
+    buffer.append("|")
+
+    // e.g. | Tables        | Are           | Cool  |
+    for (i in 0..numColumns-1) {
+        def rowValue = row.getAt(i)
+        def thisColumn = rowMap.find { k, v -> v == rowValue }.key
+        buffer.append(" ${thisColumn} | ")  
+    }
+
+    buffer.append("\n")
+
+    // e.g. | ------------- |:-------------:| -----:|
+    buffer.append("|")
+    for (i in 0..numColumns-1) {
+        buffer.append(":------:|")
+    }
+
+    buffer.append("\n")
+
+    return buffer.toString()
+}
+
+def generateRow = { def row, def numColumns -> 
+    def buffer = new StringBuilder() 
+    buffer.append("|")
+
+    // e.g. | col 3 is      | right-aligned | $1600 |
+    for (i in 0..numColumns-1) {
+        def value = row.getAt(i)
+        buffer.append(" ${value} |")
+    }
+
+    buffer.append("\n")
+
+    return buffer.toString()
+}
+
+def parseText = { def text ->
+    def data = parseCsv text
+    def numColumns = 0
+    def isHeaderGenerated = false
+
+    data.each { def row ->
+        def rowMap = row.toMap()
+        numColumns = rowMap.keySet().size()
+
+        if (! isHeaderGenerated) {
+            def outputHeader = generateHeader(row, rowMap, numColumns)
+            println outputHeader             
+            isHeaderGenerated = true 
+        }
+
+        def outputRow = generateRow(row, numColumns)
+        println outputRow 
+    } 
+}
+
+// ---------- main
+
+try {
+    if (args.length < 1) {
+        println "Check usage"
+        System.exit(-1)
+    }
+
+    def inFile = new File(args[0])
+    def inFileText = inFile.getText()
+    parseText(inFileText)
+} catch (Exception ex) {
+    System.err.println "caught exception ex: " + ex.message
+}
---
+++ groovy/csv_markdown_table/README.md
@@ -0,0 +1,12 @@
+
+### Summary 
+
+* Simple utility to create a Markdown table from a CSV
+* Uses [groovycsv](https://github.com/xlson/groovycsv)
+
+### Usage
+
+```
+groovy Converter.groovy data/input.csv > data/output.csv
+```
+
