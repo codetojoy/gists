6590971486f155b97093313b738e11280f2ffb72
Michael Easter <codetojoy@gmail.com>
2020-03-25 09:33:33 -0300

    example for PropertyResourceBundle from String

---
+++ java/property_resource_bundle_mar_2020/Example.java
@@ -0,0 +1,26 @@
+
+import java.util.*;
+import java.io.*;
+
+public class Example {
+    PropertyResourceBundle buildBundle() throws Exception {
+        StringBuilder buffer = new StringBuilder();
+        buffer.append("net.codetojoy.test=5150");
+        String bundleStr = buffer.toString();
+        InputStream targetStream = new ByteArrayInputStream(bundleStr.getBytes());
+        PropertyResourceBundle bundle = new PropertyResourceBundle(targetStream);
+        return bundle;
+    }
+
+    void go() throws Exception {
+        PropertyResourceBundle bundle = buildBundle();
+        String value = bundle.getString("net.codetojoy.test");
+        System.out.println("TRACER value: " + value);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Example example = new Example();
+        example.go();
+        System.out.println("TRACER Ready");
+    }
+}
