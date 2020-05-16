7ba5fd42db339c4fed0196221fd22b92a81097bb
Michael Easter <codetojoy@gmail.com>
2020-05-16 09:42:48 -0300

    mocking and verify

--- python/unittest/mytest.py
+++ python/unittest/mytest.py
@@ -1,5 +1,7 @@
 import unittest
 from unittest.mock import MagicMock
+from unittest.mock import patch
+
 from script import my_func
 from script import MyClass 
 from script import Foo 
@@ -11,19 +13,38 @@ class TestStringMethods(unittest.TestCase):
         self.assertEqual('foo'.upper(), 'FOO')
 
     def test_my_func(self):
+        # test 
         result = my_func(5000,150)
+
         self.assertEqual(result, 5150)
 
     def test_myclass_do_square(self):
         my_c = MyClass()
+
+        # test
         result = my_c.do_square(25)
+
         self.assertEqual(result, 625)
+
+    @patch('os.path.isfile', return_value=False)
+    def test_myclass_check_file(self, mock_isfile):
+        file = 'bogus'
+        my_c = MyClass()
+
+        # test
+        result = my_c.check_file(file)
+
+        self.assertFalse(result)
+        self.assertTrue(mock_isfile.called) 
 
     def test_foo_go(self):
         foo = Foo()
         bar = Bar()
         bar.go = MagicMock(return_value='mock value')
+
+        # test
         result = foo.go(bar)
+
         self.assertEqual(result, 'mock value')
 
 if __name__ == '__main__':
--- python/unittest/script.py
+++ python/unittest/script.py
@@ -1,10 +1,15 @@
 
+import os
+
 def my_func(x, y):
-	return x+y
+    return x+y
 
 class MyClass:
-	def do_square(self, x):
-		return x*x
+    def do_square(self, x):
+        return x*x
+
+    def check_file(self, file):
+        return os.path.isfile(file) 
 
 class Foo:
     def go(self, bar):
