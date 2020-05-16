95ab658bbd4d828167e22fd77c779ae847b545c4
Michael Easter <codetojoy@gmail.com>
2020-05-15 21:00:59 -0300

    now with mock

--- python/unittest/mytest.py
+++ python/unittest/mytest.py
@@ -1,6 +1,9 @@
 import unittest
+from unittest.mock import MagicMock
 from script import my_func
 from script import MyClass 
+from script import Foo 
+from script import Bar 
 
 class TestStringMethods(unittest.TestCase):
 
@@ -11,10 +14,17 @@ class TestStringMethods(unittest.TestCase):
         result = my_func(5000,150)
         self.assertEqual(result, 5150)
 
-    def test_do_square(self):
+    def test_myclass_do_square(self):
         my_c = MyClass()
         result = my_c.do_square(25)
         self.assertEqual(result, 625)
+
+    def test_foo_go(self):
+        foo = Foo()
+        bar = Bar()
+        bar.go = MagicMock(return_value='mock value')
+        result = foo.go(bar)
+        self.assertEqual(result, 'mock value')
 
 if __name__ == '__main__':
     unittest.main()
--- python/unittest/script.py
+++ python/unittest/script.py
@@ -5,3 +5,11 @@ def my_func(x, y):
 class MyClass:
 	def do_square(self, x):
 		return x*x
+
+class Foo:
+    def go(self, bar):
+        return bar.go()
+
+class Bar:
+    def go(self, s):
+        return s
