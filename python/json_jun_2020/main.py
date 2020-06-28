00a7bb7e7d474ff4137591bb003c42c0c410ddef
Michael Easter <codetojoy@gmail.com>
2020-06-28 16:18:32 -0300

    added exception handling

--- python/json_jun_2020/main.py
+++ python/json_jun_2020/main.py
@@ -1,15 +1,26 @@
+import sys
 import json
 from collections import namedtuple
 
-f = open("data.json", "r")
-json_str = f.read()
-json_dict = json.loads(json_str)
+def main():
+    try: 
+        f = open("data.json", "r")
+        json_str = f.read()
+        json_dict = json.loads(json_str)
 
-num_cards = json_dict["num_cards"]
-print("TRACER num_cards: " + str(num_cards))
+        num_cards = json_dict["num_cards"]
+        print("TRACER num_cards: " + str(num_cards))
 
-players = json_dict["players"]
-for player in players:
-    print("TRACER player name: " + player["name"])
+        players = json_dict["players"]
+        for player in players:
+            print("TRACER player name: " + player["name"])
 
-print("Ready.")
+        print("Ready.")
+    except: 
+        e = sys.exc_info()[0]
+        print("TRACER illegal json: " + str(e))
+        sys.exit(-1)
+
+if __name__ == "__main__":
+    # execute only if run as a script
+    main()
