b22f82ad4ec554d0a396702a385c86e0626a9271
Michael Easter <codetojoy@gmail.com>
2020-07-30 14:17:42 -0300

    some more tests

--- rust/factorial_single_thread_jul_2020/sandbox/src/main.rs
+++ rust/factorial_single_thread_jul_2020/sandbox/src/main.rs
@@ -36,3 +36,59 @@ fn main() {
 
     println!("Ready.");
 }
+
+#[allow(unused_imports)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_chunk_low_boundary() {
+        let index: u64 = 1;
+        let chunk: u64 = 10;
+        let max: u64 = 25;
+
+        // test
+        let (low, high) = get_chunk(index, chunk, max).unwrap();
+
+        assert_eq!(low, 0);
+        assert_eq!(high, 10);
+    }
+
+    #[test]
+    fn test_get_chunk_basic() {
+        let index: u64 = 2;
+        let chunk: u64 = 10;
+        let max: u64 = 25;
+
+        // test
+        let (low, high) = get_chunk(index, chunk, max).unwrap();
+
+        assert_eq!(low, 10);
+        assert_eq!(high, 20);
+    }
+
+    #[test]
+    fn test_get_chunk_high_boundary() {
+        let index: u64 = 3;
+        let chunk: u64 = 10;
+        let max: u64 = 25;
+
+        // test
+        let (low, high) = get_chunk(index, chunk, max).unwrap();
+
+        assert_eq!(low, 20);
+        assert_eq!(high, 25);
+    }
+
+    #[test]
+    fn test_get_chunk_out_of_range() {
+        let index: u64 = 4;
+        let chunk: u64 = 10;
+        let max: u64 = 25;
+
+        // test
+        let result = get_chunk(index, chunk, max);
+
+        assert_eq!(result.is_none(), true);
+    }
+}
