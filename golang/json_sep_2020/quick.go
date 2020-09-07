62733aa295b12c8a9118a9a599640264efe4a3ae
Michael Easter <codetojoy@gmail.com>
2020-09-07 10:44:54 -0300

    encode JSON in Go gist

--- golang/json_sep_2020/quick.go
+++ golang/json_sep_2020/quick.go
@@ -21,7 +21,22 @@ type Config struct {
     Players []Player `json:"players"`
 }
 
-func main() {
+func encode() {
+    p1 := Player{Name: "mozart", Strategy: "max"}
+    p2 := Player{Name: "chopin", Strategy: "min"}
+    players := []Player{p1, p2}
+
+    config := Config{NumCards: 20, NumGames: 1, IsVerbose: true, Players: players}
+
+    var jsonData []byte
+    jsonData, err := json.Marshal(config)
+    if err != nil {
+        log.Println(err)
+    }
+    fmt.Println(string(jsonData))    
+}
+
+func decode() {
     // from https://tutorialedge.net/golang/parsing-json-with-golang/
     jsonFile, err := os.Open("config.json")
     defer jsonFile.Close()
@@ -48,4 +63,14 @@ func main() {
     }
 
     fmt.Println("Ready.")
+}
+
+func main() {
+    const which = 2
+
+    if which == 1 {
+        decode() 
+    } else if which == 2 {
+        encode()
+    }
 }
