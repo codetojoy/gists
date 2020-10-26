e36f6b6b91a000662d89af6f36d946e3f19091de
Michael Easter <codetojoy@gmail.com>
2020-10-26 15:29:04 -0300

    now with JSON file and CORS wide-open

---
+++ node/parse_txt_to_json/README.md
@@ -0,0 +1,7 @@
+
+### Summary
+
+* serve some files
+* parse experiment
+* added wide-open CORS 
+
--- node/parse_txt_to_json/app.js
+++ node/parse_txt_to_json/app.js
@@ -1,9 +1,11 @@
 const express = require("express");
+const cors = require('cors');
 const request = require("request");
 
 const app = express();
 const port = 3000;
 
+app.use(cors());
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
