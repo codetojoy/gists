66d2db55ad192c1f0d04448420482eaa32f44d5c
Michael Easter <codetojoy@gmail.com>
2021-02-15 16:08:51 -0400

    simple menu

--- rust/simple_console_with_modules_feb_2021/src/main.rs
+++ rust/simple_console_with_modules_feb_2021/src/main.rs
@@ -1,8 +1,36 @@
 
 use std::io;
+use std::process;
+
+
+fn new_game() {
+    println!("TRACER new game here");
+}
+
+fn play_round() {
+    println!("TRACER play round here");
+}
+
+fn process_command(mut choice: String) {
+    println!("You chose: {}", choice);
+
+    let len = choice.len();
+    choice.truncate(len - 1);
+
+    match &choice.to_lowercase()[..] {
+        "n" => new_game(),
+        "p" => play_round(),
+        "q" => process::exit(0),
+        _ => println!("unknown command"),
+    }
+}
 
 fn main() {
     println!("Commands:");
+    println!("New game (n):");
+    println!("Play round (p):");
+    println!("Quit (q)):");
+    println!("");
 
     println!("Please input your choice:");
 
@@ -10,5 +38,5 @@ fn main() {
 
     io::stdin().read_line(&mut choice).expect("Failed to read line");
 
-    println!("You chose: {}", choice);
+    process_command(choice);
 }
