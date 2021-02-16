5b90c7a32b41e9070ecbfa19eed302c709df2c61
Michael Easter <codetojoy@gmail.com>
2021-02-16 10:43:36 -0400

    incremental update Tue 16 Feb 2021 10:43:36 AST

--- rust/simple_console_with_modules_feb_2021/src/main.rs
+++ rust/simple_console_with_modules_feb_2021/src/main.rs
@@ -2,13 +2,21 @@
 use std::io;
 use std::process;
 
+fn list_players() {
+    println!("TRACER: todo list players here");
+}
 
 fn new_game() {
-    println!("TRACER new game here");
+    println!("TRACER: todo new game here");
 }
 
 fn play_round() {
-    println!("TRACER play round here");
+    println!("TRACER: todo play round here");
+}
+
+fn exit() {
+    println!("\nRoger copy: bye for now.");
+    process::exit(0);
 }
 
 fn process_command(mut choice: String) {
@@ -18,25 +26,29 @@ fn process_command(mut choice: String) {
     choice.truncate(len - 1);
 
     match &choice.to_lowercase()[..] {
+        "l" => list_players(),
         "n" => new_game(),
         "p" => play_round(),
-        "q" => process::exit(0),
+        "q" => exit(),
         _ => println!("unknown command"),
     }
 }
 
 fn main() {
-    println!("Commands:");
-    println!("New game (n):");
-    println!("Play round (p):");
-    println!("Quit (q)):");
-    println!("");
+    loop {
+        println!("\nCommands:");
+        println!("List players (l):");
+        println!("New game (n):");
+        println!("Play round (p):");
+        println!("Quit (q)):");
+        println!("");
 
-    println!("Please input your choice:");
+        println!("Please input your choice:");
 
-    let mut choice = String::new();
+        let mut choice = String::new();
 
-    io::stdin().read_line(&mut choice).expect("Failed to read line");
+        io::stdin().read_line(&mut choice).expect("Failed to read line");
 
-    process_command(choice);
+        process_command(choice);
+    }
 }
