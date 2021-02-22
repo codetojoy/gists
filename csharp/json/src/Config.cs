2428845054ba831cfc347a8194d1f14ae042eb16
Michael Easter <codetojoy@gmail.com>
2021-02-22 15:09:08 -0400

    initial config file

---
+++ csharp/json/src/Config.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Newtonsoft.Json;
+
+namespace json
+{
+    public class Config
+    {
+        [JsonProperty("num_cards")]
+        public int NumCards { get; set; }
+        [JsonProperty("num_games")]
+        public int NumGames { get; set; }
+        [JsonProperty("is_verbose")]
+        public bool IsVerbose { get; set; }
+
+        public override string ToString() {
+            return $"# games: {NumGames} # cards: {NumCards} verbose? {IsVerbose}";
+        }
+    }
+}
--- csharp/json/src/Program.cs
+++ csharp/json/src/Program.cs
@@ -2,12 +2,12 @@
 using System.IO;
 
 using Newtonsoft.Json;
-                    
+
 namespace json
 {
     class Program
     {
-        void go()
+        void parseMovie()
         {
             using (StreamReader file = File.OpenText("movie.json"))
             {
@@ -15,18 +15,39 @@ namespace json
                 var movie = (Movie) serializer.Deserialize(file, typeof(Movie));
                 Console.WriteLine($"TRACER movie: {movie}");
             }
-            Console.WriteLine("TRACER go => OK");
+            Console.WriteLine("TRACER parseMovie => OK");
+        }
+
+        void parseConfig()
+        {
+            using (StreamReader file = File.OpenText("config.console.json"))
+            {
+                var serializer = new JsonSerializer();
+                var config = (Config) serializer.Deserialize(file, typeof(Config));
+                Console.WriteLine($"TRACER {config}");
+            }
+            Console.WriteLine("TRACER parseConfig => OK");
         }
 
         static void Main(string[] args)
         {
-            try 
+            try
             {
                 var prog = new Program();
-                prog.go();
+                var which = 2;
+
+                if (which == 1)
+                {
+                    prog.parseMovie();
+
+                }
+                else if (which == 2)
+                {
+                    prog.parseConfig();
+                }
                 Console.WriteLine("Ready.");
             }
-            catch (Exception e) 
+            catch (Exception e)
             {
                 Console.WriteLine($"caught exception: {e.Message}");
             }
