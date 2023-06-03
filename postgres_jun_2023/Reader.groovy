1d7a5c2861600c575d8aa8ee4f0f44b3937f2bce
Michael Easter <codetojoy@gmail.com>
2023-06-03 08:49:05 -0300

    incremental update Sat  3 Jun 2023 08:49:05 ADT

--- postgres_jun_2023/Reader.groovy
+++ postgres_jun_2023/Reader.groovy
@@ -9,13 +9,26 @@ import org.postgresql.*
 
 def sql = Sql.newInstance("jdbc:postgresql://127.0.0.1:5432/sandbox", "postgres","swordfish", "org.postgresql.Driver")
 
+def emitCount = { table, count ->
+    String emitFormat = "# %-20s : %d"
+    println String.format(emitFormat, table, count)
+}
+
+sql.eachRow("SELECT COUNT(*) as c FROM plan") { emitCount("plans", it.c) }
+sql.eachRow("SELECT COUNT(*) as c FROM reference") { emitCount("references", it.c) }
+sql.eachRow("SELECT COUNT(*) as c FROM customer") { emitCount("customers", it.c) }
+sql.eachRow("SELECT COUNT(*) as c FROM bridge_customer_reference") { emitCount("bcrs", it.c) }
+sql.eachRow("SELECT COUNT(*) as c FROM bridge_reference_plan") { emitCount("brps", it.c) }
+
+println "----"
+
 sql.eachRow("""
-SELECT brp.id as id from bridge_reference_plan brp
+SELECT p.name as plan_name, r.name as reference_name from bridge_reference_plan brp
 JOIN plan p on p.id = brp.plan_id
 JOIN reference r on r.id = brp.reference_id
-JOIN bridge_customer_reference bcr on bcr.reference_id = r.id
+ORDER BY p.name, r.name
 """) { row ->
-    println row
+    println "plan: ${row.plan_name} reference: ${row.reference_name}"
 }
 
 sql.close()
--- postgres_jun_2023/SQL_Populate.groovy
+++ postgres_jun_2023/SQL_Populate.groovy
@@ -22,7 +22,7 @@ insert = " INSERT INTO plan (id, name, status) VALUES (?,?,?); "
 
 10.times { i ->
     def id = 100 + i
-    def name = 'user-' + i
+    def name = 'plan-' + i
     def status = 'active'
     sql.execute(insert, [id, name, status]);
 }
@@ -33,8 +33,8 @@ insert = " INSERT INTO reference (id, name, email) VALUES (?,?,?); "
 
 10.times { i ->
     def id = 200 + i
-    def name = 'user-' + i
-    String email = "user_${i}@codetojoy.net"
+    def name = 'ref-' + i
+    String email = "ref_${i}@codetojoy.net"
     sql.execute(insert, [id, name, email]);
 }
 
@@ -44,8 +44,8 @@ insert = " INSERT INTO customer (id, name, email) VALUES (?,?,?); "
 
 10.times { i ->
     def id = 300 + i
-    def name = 'user-' + i
-    String email = "user_${i}@codetojoy.net"
+    def name = 'customer-' + i
+    String email = "customer_${i}@codetojoy.net"
     sql.execute(insert, [id, name, email]);
 }
 
