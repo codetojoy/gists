b87e8c83f320b8ad6296acadaac7ae2c629cc679
Michael Easter <codetojoy@gmail.com>
2024-12-24 11:51:30 -0400

    incremental update Tue 24 Dec 2024 11:51:30 AST

---
+++ bash/text_replace_java_dec_2024/src/main/java/net/codetojoy/model/ExampleModel.java
@@ -0,0 +1,8 @@
+
+package net.codetojoy.model;
+
+import akka.example.Actor;
+
+public class ExampleModel {
+    private static Actor AKKA_ACTOR = Actor.AkkaActor;
+}
---
+++ bash/text_replace_java_dec_2024/src/main/java/net/codetojoy/service/ExampleService.java
@@ -0,0 +1,8 @@
+
+package net.codetojoy.service;
+
+import akka.example.Actor;
+
+public class ExampleService {
+    private static Actor AKKA_ACTOR = Actor.AkkaActor;
+}
