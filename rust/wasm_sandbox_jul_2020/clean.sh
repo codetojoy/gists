5cb60ea634c0c79be860ca325f9877b8ee52285c
Michael Easter <codetojoy@gmail.com>
2026-02-14 20:42:26 -0400

    remove old project

--- rust/wasm_sandbox_jul_2020/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash 
-
-for i in {1..10}
-do
-   echo ""
-done
-
-rm -rf pkg ./www/node_modules
--- rust/wasm_sandbox_jul_2020/go.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cargo install wasm-pack
-wasm-pack build
-cd www
-npm install
-npm run start
